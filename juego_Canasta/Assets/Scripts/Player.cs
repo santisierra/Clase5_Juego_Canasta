1ef33982554467f7460178f748380a66740b1035
MaitePereira <maite.pereira.c@gmail.com>
2020-05-01 18:45:33 -0300

    Player, objetos malo y bueno

--- juego_Canasta/Assets/Scripts/Player.cs
+++ juego_Canasta/Assets/Scripts/Player.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float velovidadMov;
+
+    Vector2 movimiento;
+
+    public Rigidbody2D rb;
+
+    public float puntos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       movimiento.x = Input.GetAxisRaw("Horizontal"); 
     }
+
+    void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + movimiento * velovidadMov * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger){
+
+         if (trigger.gameObject.tag.Equals("Malo")){
+
+             puntos --;
+         }
+
+         if(trigger.gameObject.tag.Equals("Bueno")){
+
+             puntos ++;
+         }
+     }  
 }
