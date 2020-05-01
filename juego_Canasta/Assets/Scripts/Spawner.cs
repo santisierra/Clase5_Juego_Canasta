e155592cbf82fcb47bcd6c41e8b810fccf1a80a0
santisierra <santisierra2493@gmail.com>
2020-05-01 17:57:15 -0300

    spawner script + prefab

--- juego_Canasta/Assets/Scripts/Spawner.cs
+++ juego_Canasta/Assets/Scripts/Spawner.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public List<GameObject> spwanableItemsPrefab;//lista de prefabs a instanciar
+
+    private Vector3 spawnPos;//posicion donde se instancian los objetos
+
+    public Vector2 spawnTimeRange;//tiempo cuando se instanciara un nuevo objeto    x = min  y = max
+    private float spawnTime;//numero elejido al azar entre spwanTimeRange
+
+    public int maxSpawnObjectsInSceneLimit;//Contador de objetos en la escena para no crear mas
+    private GameObject spawnedObjectsContainer;//Contenedor de los objetos creados
+
     void Start()
     {
-        
+        spawnTime = Mathf.Round(Random.Range(spawnTimeRange.x, spawnTimeRange.y));// inicia el primer timer
+        spawnedObjectsContainer = new GameObject("spawnedObjectsContainer");// Crea el cotenedor de objetos
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+//if gamerunning
+       
+        spawnTime -= Time.deltaTime;//resta tiemp al spwantime
+
+        if (spawnTime <= 0.0f && spawnedObjectsContainer.transform.childCount < maxSpawnObjectsInSceneLimit)//cuando spwantime es menor o igual a 0 y si la cantidad de objetos en la escena no supera el limite
+        {
+            //genero una nueva posicion donde se instancia el proximo objeto entre un valor aleatorio en el largo de la pantalla y la constante del alto
+            spawnPos = Camera.main.ScreenToWorldPoint(new Vector3( Random.Range(0, Screen.width), Screen.height, 0));
+            //le sumo 1 al y para que aparescan fuera de la pantalla
+            spawnPos = new Vector3(spawnPos.x, spawnPos.y + 1, spawnPos.z);
+
+            //Instance un objeto al azar en la lista spwanableItemsPrefab. Elije de forma aleatoria entre 0 y la cantidad de objetos que hay en la lista
+            GameObject spawnedObject = Instantiate(spwanableItemsPrefab [Random.Range(0, spwanableItemsPrefab.Count)] , spawnPos , Quaternion.identity , spawnedObjectsContainer.transform);
+
+            // pide un nuevo valor para spwantime y lo redondea
+            spawnTime = Mathf.Round(Random.Range(spawnTimeRange.x, spawnTimeRange.y)); 
+        }
     }
 }
